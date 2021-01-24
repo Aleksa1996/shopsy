e147a97b90e2b02974c5e4afdbfb4bb7a233ef7b
Aleksa Jovanovic <aleksa.j.1996@gmail.com>
2021-01-24 02:26:47 +0100

    adding behat + mink + phpunit

--- config/bundles.php
+++ config/bundles.php
@@ -5,10 +5,10 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    // K911\Swoole\Bridge\Symfony\Bundle\SwooleBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     PixelFederation\DoctrineResettableEmBundle\PixelFederationDoctrineResettableEmBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
 ];
--- src/Shopsy/IdentityAccess/Test/Domain/Model/Identity/PHPUnit/UserTest.php
+++ src/Shopsy/IdentityAccess/Test/Domain/Model/Identity/PHPUnit/UserTest.php
@@ -2,7 +2,9 @@
 
 namespace App\Shopsy\IdentityAccess\Test\Infrastructure\Persistence\Doctrine\Identity\PHPUnit;
 
+use App\Common\Domain\Id;
 use PHPUnit\Framework\TestCase;
+use App\Shopsy\IdentityAccess\Main\Domain\Model\Access\Role;
 use App\Shopsy\IdentityAccess\Main\Domain\Model\Identity\User;
 use App\Shopsy\IdentityAccess\Main\Domain\Model\Identity\UserId;
 use App\Shopsy\IdentityAccess\Main\Domain\Model\Identity\UserEmail;
@@ -23,11 +25,88 @@ class UserTest extends TestCase
             new UserFullName('Aleksa Jovanovic'),
             new UserUsername('aleksa'),
             new UserEmail('aleksa@gmail.com'),
-            new UserPassword('hella'),
+            new UserPassword('pass123'),
             new UserActive(true),
             null
         );
 
         $this->assertEquals('Aleksa Jovanovic', $user->getFullName()->getFullName());
+        $this->assertEquals('pass123', $user->getPassword()->getPassword());
+    }
+
+    /**
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $user = new User(
+            new UserId(),
+            new UserFullName('Aleksa Jovanovic'),
+            new UserUsername('aleksa'),
+            new UserEmail('aleksa@gmail.com'),
+            new UserPassword('pass123'),
+            new UserActive(true),
+            null
+        );
+
+        $active = false;
+        $email = 'aleksaa@gmail.com';
+
+        $user->setActive(new UserActive($active));
+        $user->setEmail(new UserEmail($email));
+
+        $this->assertEquals($active, $user->getActive()->getActive());
+        $this->assertEquals($email, $user->getEmail()->getEmail());
+    }
+
+    /**
+     * @return void
+     */
+    public function testRoles()
+    {
+        $user = new User(
+            new UserId(),
+            new UserFullName('Aleksa Jovanovic'),
+            new UserUsername('aleksa'),
+            new UserEmail('aleksa@gmail.com'),
+            new UserPassword('pass123'),
+            new UserActive(true),
+            null
+        );
+
+        $role1 = new Role(new Id(), 'User Role', 'ROLE_USER', true);
+
+        $user->attachRole($role1);
+        $user->attachRole($role1);
+
+        $this->assertCount(1, $user->getRoles());
+
+        $user->detachRole($role1);
+
+        $this->assertCount(0, $user->getRoles());
+
+        $role2 = new Role(new Id(), 'Admin Role', 'ROLE_ADMIN', true);
+
+        $user->attachRole($role1);
+        $user->attachRole($role1);
+        $user->attachRole($role2);
+        $user->attachRole($role2);
+
+        $this->assertIsArray($user->getRoles());
+
+        $this->assertCount(2, $user->getRoles());
+        $this->assertTrue($user->hasRole($role2));
+        $this->assertTrue($user->hasRoleIdentifier($role1->getIdentifier()));
+
+        $user->detachAllRoles();
+
+        $this->assertCount(0, $user->getRoles());
+        $this->assertFalse($user->hasRole($role1));
+        $this->assertFalse($user->hasRoleIdentifier($role1->getIdentifier()));
+
+        $user->setRoles([$role1]);
+
+        $this->assertCount(1, $user->getRoles());
+        $this->assertContainsOnlyInstancesOf(Role::class, $user->getRoles());
     }
 }
